48fb57c9efe62f7ef1bea1a6f834ba46bc8b75c5
dasbd72 <twbd723@gmail.com>
2022-12-31 12:18:06 +0800

    PR fifo

--- push-relabel.cc
+++ push-relabel.cc
@@ -10,6 +10,7 @@
 #include "graph.hh"
 #include "utility.hh"
 
+namespace PR {
 struct Data {
     int V;
     int S;
@@ -21,41 +22,42 @@ struct Data {
     int *excess;
     int *residual;
     int *height;
+    int fifoHead;
+    int fifoTail;
+    int fifoSize;
+    int *fifo;
 };
 
-void preflow(Data *data) {
-    int V = data->V;
-    int S = data->S;
-    memset(data->excess, 0, sizeof(int) * V);
-    memset(data->height, 0, sizeof(int) * V);
-    data->height[S] = V;
-    data->excess[S] = INT_MAX;
-    for (int i = 0; i < data->nedge[S]; i++) {
-        int v = data->edge[S * V + i];
-        int delta = data->residual[S * V + v];
-        if (delta) {
-            data->residual[S * V + v] = 0;
-            data->residual[v * V + S] += delta;
-            data->excess[S] -= delta;
-            data->excess[v] += delta;
-        }
-    }
+inline void fifoPush(Data *data, int u) {
+    data->fifo[data->fifoTail] = u;
+    data->fifoTail = (data->fifoTail + 1) % data->V;
+    data->fifoSize++;
+}
+
+inline int fifoPop(Data *data) {
+    int retVal = data->fifo[data->fifoHead];
+    data->fifoHead = (data->fifoHead + 1) % data->V;
+    data->fifoSize--;
+    return retVal;
 }
 
 // applies if excess[u] > 0, residual[u * V + v] > 0, and height[u] = height[v] + 1
-void push(Data *data, int u, int v) {
+inline void push(Data *data, int u, int v) {
     int V = data->V;
     int delta = min(data->excess[u], data->residual[u * V + v]);
     if (delta) {
         data->residual[u * V + v] -= delta;
         data->residual[v * V + u] += delta;
         data->excess[u] -= delta;
+        if (data->excess[v] == 0 && v != data->S && v != data->T) {
+            fifoPush(data, v);
+        }
         data->excess[v] += delta;
     }
 }
 
 // applies if excess[u] > 0 and if height[u] < height[v] for all (u,v) residual[u * V + v] > 0
-void relabel(Data *data, int u) {
+inline void relabel(Data *data, int u) {
     int V = data->V;
     int minHeight = INT_MAX;
     for (int v = 0; v < V; v++) {
@@ -65,7 +67,7 @@ void relabel(Data *data, int u) {
     data->height[u] = minHeight + 1;
 }
 
-int getExcess(Data *data) {
+inline int getExcess(Data *data) {
     int V = data->V;
     int S = data->S;
     int T = data->T;
@@ -76,33 +78,69 @@ int getExcess(Data *data) {
     }
     return -1;
 }
+}  // namespace PR
+using namespace PR;
 
 void PushRelabel(Graph *graph, int *flow) {
     Data *data = (Data *)malloc(sizeof(Data));
     int V = data->V = graph->V;
     int S = data->S = graph->S;
     int T = data->T = graph->T;
-    int ncpus = data->ncpus = graph->ncpus;
+    data->ncpus = graph->ncpus;
     data->capacity = (int *)malloc(sizeof(int) * V * V);
     data->edge = (int *)malloc(sizeof(int) * V * V);
     data->nedge = (int *)malloc(sizeof(int) * V);
     data->excess = (int *)malloc(sizeof(int) * V);
     data->residual = (int *)malloc(sizeof(int) * V * V);
     data->height = (int *)malloc(sizeof(int) * V);
+    data->fifoHead = 0;
+    data->fifoTail = 0;
+    data->fifoSize = 0;
+    data->fifo = (int *)malloc(sizeof(int) * V);
 
-    memset(data->capacity, 0, sizeof(int) * V * V);
-    for (int r = 0; r < V; r++) {
-        data->nedge[r] = graph->edge[r].size();
-        for (int i = 0; i < data->nedge[r]; i++) {
-            data->edge[r * V + i] = graph->edge[r][i].first;
-            data->capacity[r * V + graph->edge[r][i].first] = graph->edge[r][i].second;
+    TIMING_START(_init);
+    for (int u = 0; u < V; u++) {
+        for (int v = 0; v < V; v++) {
+            data->capacity[u * V + v] = 0;
+            data->residual[u * V + v] = 0;
         }
     }
-    memcpy(data->residual, data->capacity, sizeof(int) * V * V);
+    for (int u = 0; u < V; u++) {
+        data->nedge[u] = graph->edge[u].size();
+        for (int i = 0; i < data->nedge[u]; i++) {
+            int v = graph->edge[u][i].first;
+            data->edge[u * V + i] = v;
+            data->capacity[u * V + v] = graph->edge[u][i].second;
+            data->residual[u * V + v] = graph->edge[u][i].second;
+        }
+    }
+    for (int u = 0; u < V; u++) {
+        data->excess[u] = 0;
+        data->height[u] = 0;
+    }
+    TIMING_END(_init);
 
-    preflow(data);
-    int u;
-    while ((u = getExcess(data)) != -1) {
+    TIMING_START(_preflow);
+    data->height[S] = V;
+    data->excess[S] = INT_MAX;
+    for (int i = 0; i < data->nedge[S]; i++) {
+        int v = data->edge[S * V + i];
+        int delta = data->residual[S * V + v];
+        if (delta) {
+            data->residual[S * V + v] = 0;
+            data->residual[v * V + S] += delta;
+            data->excess[S] -= delta;
+            if (v != T) {
+                fifoPush(data, v);
+            }
+            data->excess[v] += delta;
+        }
+    }
+    TIMING_END(_preflow);
+
+    TIMING_START(_innerPushRelabel);
+    while (data->fifoSize) {
+        int u = fifoPop(data);
         bool isMin = true;
         for (int v = 0; v < V; v++) {
             if (data->residual[u * V + v] > 0) {
@@ -115,14 +153,20 @@ void PushRelabel(Graph *graph, int *flow) {
         if (isMin) {
             relabel(data, u);
         }
-    }
-
-    for (int r = 0; r < V; r++) {
-        for (int i = 0; i < data->nedge[r]; i++) {
-            int c = data->edge[r * V + i];
-            flow[r * V + c] = data->capacity[r * V + c] - data->residual[r * V + c];
+        if (data->excess[u] > 0) {
+            fifoPush(data, u);
         }
     }
+    TIMING_END(_innerPushRelabel);
+
+    TIMING_START(_flow);
+    for (int u = 0; u < V; u++) {
+        for (int i = 0; i < data->nedge[u]; i++) {
+            int v = data->edge[u * V + i];
+            flow[u * V + v] = data->capacity[u * V + v] - data->residual[u * V + v];
+        }
+    }
+    TIMING_END(_flow);
 
     free(data->capacity);
     free(data->edge);
@@ -130,5 +174,6 @@ void PushRelabel(Graph *graph, int *flow) {
     free(data->excess);
     free(data->residual);
     free(data->height);
+    free(data->fifo);
     free(data);
 }
